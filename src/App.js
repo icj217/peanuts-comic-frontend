6b1bd12a92d35c70352bf7a7382f3473f7f96d55
Craig Burdulis <craig.burdulis@jefferson.edu>
2019-09-27 16:03:31 -0400

    Initial create-react-app structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./logo.png" className="App-logo" alt="Charlie Brown" />
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
