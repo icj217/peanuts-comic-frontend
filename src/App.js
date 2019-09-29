838b46fb790d659777c0f4ddcaad2138b51bd8ea
Craig Burdulis <craigburdulis@CJ-LT.local>
2019-09-29 12:14:49 -0400

    Added snoopy image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import CharlieBrown from './logo.svg'
+import CharlieBrown from './charlie_brown.svg'
+import Snoopy from './snoopy.svg'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={CharlieBrown} className="App-logo" alt="Charlie Brown" />
-        <p>
-          Hello World!
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: CharlieBrown,
+      imageName: 'Charlie Brown'
+    }
+  }
+  changeImage = (state) => {
+    const image = state.image === CharlieBrown ? Snoopy : CharlieBrown
+    const imageName = state.image === CharlieBrown ? 'Snoopy' : 'Charlie Brown'
+    this.setState({ image, imageName })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={this.state.image} alt={this.state.imageName} className="App-logo" onClick={() => this.changeImage(this.state)} />
+          <p>
+            Hello World!
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
